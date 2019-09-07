cf2df43c871e41b14f27543c74b5e60d93f90ab1
Antonio Sarosi <sarosiantonio@gmail.com>
2019-09-08 01:33:16 +0200

    Fixed resizing window bug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(() => {
 
     // Create default array
-
     const defaultArray = [2, 8, 1, 6, 3, 7, 5, -1, 4, -3, -5];
     let arrayContainer = $('<div></div>').addClass('array-container');
 
@@ -10,16 +9,18 @@ $(document).ready(() => {
         let element = $('<div></div>').addClass('array-element');
         arrayContainer.append(element.append(value));
     }
-
     $('section.animation-zone').append(arrayContainer);
 
     // Center default array
-
-    $(arrayContainer).css(
-        'left',
-        $('section.animation-zone').width() / 2
-        - $(arrayContainer).width() / 2 + 'px'
-    );
+    function centerArray() {
+        $(arrayContainer).css(
+            'left',
+            $('section.animation-zone').width() / 2
+            - $(arrayContainer).width() / 2 + 'px'
+        );
+    }
+    centerArray();
+    $(window).resize(centerArray);
 
     // Sort button
 
